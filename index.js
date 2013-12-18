c68aed0ad1a8485bc7dfa3b2cec41cb715490c64
Chris Grimmett <chris@grimtech.net>
2013-12-18 09:26:25 +0000

    separated auth to api and middleware dirs

--- index.js
+++ index.js
@@ -11,17 +11,15 @@ var express = require('express'),
     nconf = require('nconf'),
     redis = require('redis'),
     //jsonify = require('redis-jsonify'),
-    passport = require('passport'),
-    TwitterStrategy = require('passport-twitter').Strategy;
-
-    var middleware = require('./source/middleware');
+//    passport = require('passport'),
+    middleware = require('./source/middleware');
 
 
-// api endpoints
-//require('./source/api/auth')(app);
+
+
 
 // connect to redis
-var client = redis.createClient(null, null, {"retry_max_delay": "180000"});
+var rclient = redis.createClient(null, null, {"retry_max_delay": "180000"});
 
 // log errors
 
@@ -37,6 +35,7 @@ nconf.defaults({
 });
 
 // configure the app
+app.set('TITLE', nconf.get('TITLE'));
 app.set('PORT', nconf.get('PORT'));
 app.set('SESSION_SECRET', nconf.get('SESSION_SECRET'));
 app.set('TWITTER_CONSUMER_KEY', nconf.get('TWITTER_CONSUMER_KEY'));
@@ -45,63 +44,15 @@ app.set('TWITTER_CONSUMER_SECRET', nconf.get('TWITTER_CONSUMER_SECRET'));
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.session({ secret: app.get('SESSION_SECRET') }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(express.logger('dev')); // @todo for production,  change this
-        client.on("error", function(err) {
+        rclient.on("error", function(err) {
         console.log("Error " + err);
 });
 app.use(app.router);
 
 
-
-
-// configure passport
-passport.use(new TwitterStrategy({
-        consumerKey: app.get('TWITTER_CONSUMER_KEY'),
-        consumerSecret: app.get('TWITTER_CONSUMER_SECRET'),
-        callbackURL: "http://dwane.co:9090/api/auth/twitter/callback"
-    },
-
-    // when user successfully authenticates with twitter, do this:
-    function(token, tokenSecret, profile, done) {
-//        User.findOrCreate(profile.id, function(err, user) {
-//        if (err) { return done(err); }
-//              console.log('ima done');
-              done(null, profile);
-//        });
-    })
-)
-
-// serialize user object to the session
-// this means the 
-passport.serializeUser(function(user, done) {
-    console.log('ima serializeing');
-    done(null, user);
-});
-
-// pull the cookie from the user's browser
-// using the cookie, find who it is that is visiting
-// pull that user's info from the db
-passport.deserializeUser(function(user, done) {
-    console.log('ima deserializin');
-    done(null, user);
-});
-
-app.get('/', function(req, res) {
-    if(req.user) {
-        res.render('index.html');
-//        res.send(req.user);
-    } else {
-        res.send('<a href="/api/auth/twitter">Sign in with Twitter</a>');
-    }
-});
-
-
-app.get('/api/auth/twitter', passport.authenticate('twitter'));
-app.get('/api/auth/twitter/callback',
-	passport.authenticate('twitter', { successRedirect: '/',
-                                           failureRedirect: '/login' }));
 
 
 
@@ -123,6 +74,7 @@ nunjucksEnv.express(app);
 //     });
 // });
 
+
 // serve these static directories
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static(path.join(__dirname, '/public/css')));
@@ -137,5 +89,132 @@ app.use(express.static(path.join(__dirname, '/public/vendor/x-editable/bootstrap
 app.use(express.static(path.join(__dirname, '/public/vendor/x-editable/bootstrap3-editable/img')));
 app.use(express.static(path.join(__dirname, '/public/vendor/x-editable/bootstrap3-editable/js')));
 
+
+// api endpoints
+require('./source/api/auth')(app);
+//require('./source/api/user')(app);
+
+
 console.log('App runnin');
 server.listen(app.get('PORT'));
+
+
+
+//
+//
+//
+
+
+// function User() {
+//     // connect to redis
+
+
+// };
+
+// User.prototype.findOrCreate = function(id, fn) {
+//     // finds the specified user id. if specified user id doesn't exit, create the user
+
+//     var rclient = redis.createClient(null, null, {"retry_max_delay": "180000"});    
+    
+// DATABASE
+//
+// account_types:
+//   0: free
+//   1: premium
+//
+// idea: users have a 'version' configuration, where they can choose the version of dawan that they use
+//
+// 'wans' is the number of WAN ports the user has on their router
+// 'update_time' is an array of time in milliseconds that the user's client will contact the master server.
+//     Each array element is for the user's different WANs. ie each WAN has it's own update interval.
+
+
+
+// id = INCR next.user.id
+
+// SET user/$id name john
+
+// GET user/$id name
+// "john"
+
+
+
+// SET users:$id id
+// SET users:$id:account:name 'george henry'
+// SET users:$id:account:email george@example.com
+// SET users:$id:account:password rosebud
+// SET users:$id:account:type 1
+// SET users:$id:config:wans 1
+// SET users:$id:config:update.time '3000 6000'
+
+
+
+
+// client.set("skey", "hello worldy", redis.print);
+// //client.hset("hkey", "hashtest 1", "someval", redis.print);
+// client.mset(["test keys 1", "test val 1", "test keys 2", "test val 2"], function (err, res) {});
+// client.hset(["hkey", "hashtestt 2", "some other valuee"], redis.print);
+// client.hkeys("hkey", function(err, replies) {
+//     console.log(replies.length + " replies: ");
+
+//     replies.forEach(function (reply, i) {
+//         console.log("    " + i + ": " + reply);
+//     });
+//     client.quit();
+// });
+
+
+
+
+
+
+    // client.set("users", users, function(err, result) {
+    // client.get("users", function(err, result) {
+       // console.dir(result);                                                                                                                 
+    //  });
+    // });
+    //};
+
+//module.exports = User;var redis = require('redis');
+
+
+// var User = {};
+
+// //User.prototype.findOrCreate = function(id, fn) {
+
+// User.findOrCreate = function(id, fn) {
+//     // finds the specified user id. if specified user id doesn't exit, create the user
+
+//     var rclient = redis.createClient(null, null, {"retry_max_delay": "180000"});    
+
+//     rclient.get('user/' + id, function(err, reply) {
+//         if (err) throw err;
+//         if (reply) {
+//             console.log('REDIS: reply received: ');
+//             console.dir(reply);
+//             reply(null, user);
+// 	}
+//     });
+
+//     rclient.quit();
+//     fn(null, user);
+
+// }
+
+
+
+
+// DATABASE
+//
+// account_types:
+//   0: free
+//   1: premium
+//
+// idea: users have a 'version' configuration, where they can choose the version of dawan that they use
+//
+// 'wans' is the number of WAN ports the user has on their router
+// 'update_time' is an array of time in milliseconds that the user's client will contact the master server.
+//     Each array element is for the user's different WANs. ie each WAN has it's own update interval.
+
+
+
