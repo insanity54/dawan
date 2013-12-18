5dde9af07f1bdea100eac8e819c8d073660e33fd
Chris Grimmett <chris@grimtech.net>
2013-12-18 09:28:57 +0000

    mvc views dir

---
+++ public/js/views/wans.view.js
@@ -0,0 +1,68 @@
+define([
+  'require',
+  'jquery',
+  'underscore',
+  'bootstrap',
+  'nunjucks',
+  'backbone',
+  'bootstrap-editable'
+], function(require) {
+
+    var view = {};
+
+    /*
+     * ATTRIBUTES
+     */
+
+    view.className = 'wan'
+
+
+
+    /*
+     * EVENTS
+     */
+
+    view.events = {
+        'click .add' : 'onAddWan',
+        'keypress .edit' : 'onEdit'
+    }
+
+
+
+    /*
+     * EVENT HANDLERS
+     */
+
+    view.onAddWan = function() {
+        console.log('add wan event');
+    }
+
+    view.onEdit = function() {
+        console.log('edit event');
+    }
+
+    
+
+    /*
+     * PUBLIC CLASS METHODS
+     */
+    view.render = function() {
+        console.log('wans view rendering');
+        this.$el.html( nunjucks.render( 'wans.html', attributes ));
+        return this;
+    }
+
+
+    /*
+     * VIEW CONSTRUCTOR AND INITIALIZATION
+     */
+    view.initialize = function(options) {
+        var that = this;
+        _.bindAll(this);
+	
+        this.model = options.model;
+        console.log('model', this.model.id)
+        if(!this.model.isNew()) {
+            // model does not have an id yet
+            console.log('bound ', this.model.id)
+            
