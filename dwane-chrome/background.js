745bb05bbc93451f7a1c276292c95477bc172662
Chris Grimmett <chris@grimtech.net>
2014-05-09 15:41:27 -0700

    progress

--- dwane-chrome/background.js
+++ dwane-chrome/background.js
@@ -1,14 +1,26 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('control.html', {
-	id: 'controlpanel',
-	'bounds': {
-	    'width': 600,
-	    'height': 400
-	}
-    });
-});
-
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.local.set(object items, function callback);
+});
+
+
+/**
+ * Create control panel window when app is started
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+	if (controlWindow && !controlWindow.contentWindow.closed) {
+		controlWindow.focus();
+	} else {
+		chrome.app.window.create('control.html', {
+		    id: "controlpanel",
+		    bounds: {
+			width: 500,
+			height: 309,
+			left: 0
+		    }
+		},
+		    function(w) {
+			commandWindow = w;
+		    });
+	}
 });
